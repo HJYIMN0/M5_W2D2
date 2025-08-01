071ee89f89206f80f3ddd97a283beb1cac5cc723
HJYIMN0 <sim0allaseconda@gmail.com>
2025-08-01 16:38:59 +0200

    Espletamento esercizio 2

---
+++ Assets/Scripts/NM_EnemyController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class NM_EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform[] _waypoints;
+    [SerializeField] private float _distance = 1f;
+    [SerializeField] private float _waitingTime = 2f;
+    [SerializeField] private float _radius = 5f;
+    [SerializeField] private LayerMask _playerLayer;
+
+
+    private GameObject _player;
+
+    private enum ENEMYSTATE { PATROLLING, FOLLOWING }
+    private ENEMYSTATE _enemyState = ENEMYSTATE.PATROLLING;
+
+    private NavMeshAgent _agent;
+    private int _index;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        if (_agent == null) Debug.Log("Manca il NavMeshAgent!");
+    }
+
+    private void Start()
+    {
+        if (_agent == null || _waypoints == null || _waypoints.Length == 0)
+        {
+            Debug.Log("There's something wrong!");
+            return;
+        }
+
+
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _enemyState = ENEMYSTATE.PATROLLING;
+        _index = 0;
+        StartCoroutine("ChoosePath");
+
+    }
+
+    public void Update()
+    {
+        if (Physics.CheckSphere(transform.position, _radius, _playerLayer))
+        {
+            _enemyState = ENEMYSTATE.FOLLOWING;
+            FollowPlayer();
+        }
+        else _enemyState = ENEMYSTATE.PATROLLING;
+
+        Debug.Log(_enemyState);
+    }
+    IEnumerator ChoosePath()
+    {
+        while (_enemyState == ENEMYSTATE.PATROLLING)
+        {
+            while (true)
+            {
+                NavMeshPath path = new();
+
+                while (_index < _waypoints.Length)
+                {
+                    Vector3 target = _waypoints[_index].position;
+
+                    _agent.CalculatePath(target, path);
+                    _agent.SetPath(path);
+                    _agent.destination = target;
+
+                    // Aspetta finché il path non è calcolato e il nemico non è arrivato
+                    while (_agent.remainingDistance > _distance)
+                    {
+                        yield return null; // aspetta il prossimo frame
+                    }
+
+                    // Arrivato
+                    _agent.ResetPath();
+                    yield return new WaitForSeconds(_waitingTime);
+                    _index++;
+                }
+
+                _index = 0;
+            }
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        if (_enemyState != ENEMYSTATE.FOLLOWING)
+        {
+            Debug.Log("Non sto inseguendo nessuno!");
+            return;
+        }
+        else
+        {
+            _agent.ResetPath();
+            NavMeshPath path = new();
+            Vector3 target = _player.transform.position;
+
+            _agent.CalculatePath(target, path);
+            _agent.SetPath(path);
+            _agent.destination = target;
+
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+
+}
--- Assets/Scripts/NM_PlayerController.cs
+++ Assets/Scripts/NM_PlayerController.cs
@@ -7,11 +7,14 @@ using UnityEngine.AI;
 public class NM_PlayerController : MonoBehaviour
 {
     [SerializeField] private Camera _cam;
+    [SerializeField] private float _desiredDistance = 0.5f;
 
     private LineRenderer _lineRenderer;
 
     private NavMeshAgent _agent;
     private bool _isLineRender;
+
+    private Vector3 _desiredPos;
 
     
 
@@ -31,6 +34,9 @@ public class NM_PlayerController : MonoBehaviour
             CalculatePath();
         }
 
+        if (Vector3.Distance(_desiredPos, _agent.transform.position) <= _desiredDistance)
+            _isLineRender = false;
+
         _lineRenderer.enabled = _isLineRender;
     }
 
@@ -40,21 +46,54 @@ public class NM_PlayerController : MonoBehaviour
         Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            _agent.CalculatePath(hit.point, path);
-            _agent.destination = hit.point;
+            if (!Input.GetButton("Fire3"))
+            {
+                _agent.CalculatePath(hit.point, path);
+                _agent.destination = hit.point;
+                _desiredPos = _agent.destination;
+
+            }
+            else _agent.ResetPath();
         }
 
         if (path != null)
         {
             _isLineRender = true;
-            //_lineRenderer.SetPosition(0, hit.point);
-            Vector3[] linepoint = new Vector3[2];
-            linepoint[0] = hit.point;
-            linepoint[1] = _agent.transform.position;
-            _lineRenderer.SetPositions(linepoint);
+            DrawPath(hit);
+            EvaluatePathState(path);
         }
         else _isLineRender = false;
+    }
 
+    private void DrawPath(RaycastHit hit)
+    {
+        Vector3[] linePoints = new Vector3[2];
+        linePoints[0] = hit.point;
+        linePoints[1] = _agent.transform.position;
+        _lineRenderer.SetPositions(linePoints);
+    }
+
+    private void EvaluatePathState(NavMeshPath path)
+    {
+        if (path.status == NavMeshPathStatus.PathComplete)
+        {
+            _lineRenderer.startColor = Color.green;
+            _lineRenderer.endColor = Color.green;
+            _lineRenderer.material.color = Color.green;
+        }
+        else if (path.status == NavMeshPathStatus.PathPartial)
+        {
+            _lineRenderer.startColor = Color.yellow;
+            _lineRenderer.endColor = Color.yellow;
+            _lineRenderer.material.color = Color.yellow;
+
+        }
+        else if (path.status != NavMeshPathStatus.PathInvalid)
+        {
+            _lineRenderer.startColor = Color.red;
+            _lineRenderer.endColor = Color.red;
+            _lineRenderer.material.color = Color.red;
+        }
     }
 
 }
